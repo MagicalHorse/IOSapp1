539a0482c85a1cbe5520b3343d2f7878a17cabc7
liyuhbs <626235732@qq.com>
2015-11-20 17:40:28 +0800

    新增了搜索ui

---
+++ joybar/joybar/败家模式/搜索/SearchDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchDetailsViewController.h
+//  joybar
+//
+//  Created by joybar on 15/11/20.
+//  Copyright © 2015年 卢兴. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface SearchDetailsViewController : BaseViewController
+
+@end
