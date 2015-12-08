8963d486b5e64c280ec83e6cd58ea0a305475b0a
liyuhbs <626235732@qq.com>
2015-12-08 10:24:59 +0800

    修改icon

--- joybar/joybar/养家模式/BuyerTabBarViewController.m
+++ joybar/joybar/养家模式/BuyerTabBarViewController.m
@@ -98,9 +98,9 @@
     //TabBarItem的title
     NSArray *title = @[@"",@"",@"",@"",@""];
     //TabBar上Button在Normal状态下的图片
-    NSArray *tabBarPress = @[@"shouye2",@"jiangli2",@"fabu点击",@"xiaoxi2",@"geren2"];
+    NSArray *tabBarPress = @[@"shouye2",@"jiangli2",@"fabu点击",@"xiaoxi2",@"个人"];
     //TabBar上Button在Selected状态下的图片
-    NSArray * tabBarImage = @[@"shouye1",@"jiangli1",@"fabu常态",@"xiaoxi1",@"geren1"];
+    NSArray * tabBarImage = @[@"shouye1",@"jiangli1",@"fabu常态",@"xiaoxi1",@"个人1"];
     self.btnArray = [NSMutableArray array];
     //创建TabBar上的Button和Label
     UIImageView *bgImgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, kScreenWidth, self.tabBar.frame.size.height)];
