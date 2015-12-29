34b44a923c2cf4b241c7e80bd3111379e3e77797
liyuhbs <liyuhbs@163.com>
2015-12-29 16:51:59 +0800

    添加专柜买手的退货页面

---
+++ joybar/joybar/败家模式/申请退款/CusRefundPriceVipController.h
@@ -0,0 +1,13 @@
+//
+//  CusRefundPriceVipController.h
+//  joybar
+//
+//  Created by joybar on 15/12/29.
+//  Copyright © 2015年 卢兴. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface CusRefundPriceVipController : BaseViewController
+
+@end
